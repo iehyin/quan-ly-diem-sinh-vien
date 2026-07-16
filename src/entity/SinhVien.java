e8589079d5fba960114fe60ca5ee73fe854ac6c9
iehyin <maiithiyennhiii@gmail.com>
2026-07-16 12:50:32 +0700

    Thêm class Nguoi và hoàn thiện entity

--- src/entity/SinhVien.java
+++ src/entity/SinhVien.java
@@ -1,4 +1,31 @@
 package entity;
 
-public class SinhVien {
+public class SinhVien extends Nguoi{
+        private String lop;
+        private String maSv;
+
+    public SinhVien()  {
+        }
+
+    public SinhVien(String hoTen, String ngaySinh, String lop, String maSv) {
+            super(hoTen, ngaySinh);
+            this.lop = lop;
+            this.maSv = maSv;
+        }
+
+    public String getLop() {
+        return lop;
+    }
+
+    public void setLop(String lop) {
+        this.lop = lop;
+    }
+
+    public String getMaSv() {
+        return maSv;
+    }
+
+    public void setMaSv(String maSv) {
+        this.maSv = maSv;
+    }
 }
