37cdd960f87de72f58be0e6a1356756825e1c091
nguyentvi0707 <nguyentvi0707@gmail.com>
2026-07-20 10:21:43 +0700

    goc

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,6 @@ import entity.MonHoc;
 
 public class Main {
     public static void main(String[] args) {
-// Thay vì load dữ liệu cũ, ta luôn tạo mới một storage trống mỗi khi chạy
-        DataStorage storage = new DataStorage();
 
-        // Tự động nạp sẵn 3 môn học giả định vào kho dữ liệu mới này
-        storage.themMonHoc(new entity.MonHoc("S01", "Lập trình WEB", 3));
-        storage.themMonHoc(new entity.MonHoc("S02", "Lập trình hướng đối tượng", 2));
-        storage.themMonHoc(new entity.MonHoc("S03", "Toán cao cấp", 1));
-
-        // Khởi chạy Menu chương trình
-        ConsoleUI ui = new ConsoleUI(storage);
-        ui.start();
     }
 }
